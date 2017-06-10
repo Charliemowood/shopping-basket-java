2de995ccce32a6442e10e1b239706550096296ef
Charlie Wood <charliemowood@gmail.com>
2017-06-10 22:49:57 +0100

    Pass test doesItemClassExist

--- app/src/test/java/com/codeclan/example/shoppingbasket/ItemTest.java
+++ app/src/test/java/com/codeclan/example/shoppingbasket/ItemTest.java
@@ -1,10 +1,19 @@
 package com.codeclan.example.shoppingbasket;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by home on 6/10/17.
  */
 public class ItemTest {
+    Item item = new Item();
+
+    @Test
+    public void doesItemClassExist() {
+        assertNotNull(item);
+    }
+
 
 }
