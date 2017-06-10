abd5faf264c911a319608ea96eddfc705a9ad27f
Charlie Wood <charliemowood@gmail.com>
2017-06-10 22:46:34 +0100

    Pass test doesBasketClassExist

--- app/src/test/java/com/codeclan/example/shoppingbasket/BasketTest.java
+++ app/src/test/java/com/codeclan/example/shoppingbasket/BasketTest.java
@@ -1,10 +1,17 @@
 package com.codeclan.example.shoppingbasket;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by home on 6/10/17.
  */
 public class BasketTest {
+    Basket basket = new Basket();
+    @Test
+    public void doesBasketClassExist() {
+        assertNotNull(basket);
+    }
 
 }
